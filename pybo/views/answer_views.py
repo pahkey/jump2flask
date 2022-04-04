5108f4fe5d62da68ecaa039fcabbf8a7d483cb11
pahkey <pahkey@gmail.com>
2022-04-04 19:50:29 +0900

    2-10

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request
+from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
 
 from pybo import db
+from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
@@ -11,9 +12,12 @@ bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=('POST',))
 def create(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    content = request.form['content']
-    answer = Answer(content=content, create_date=datetime.now())
-    question.answer_set.append(answer)
-    db.session.commit()
-    return redirect(url_for('question.detail', question_id=question_id))
+    if form.validate_on_submit():
+        content = request.form['content']
+        answer = Answer(content=content, create_date=datetime.now())
+        question.answer_set.append(answer)
+        db.session.commit()
+        return redirect(url_for('question.detail', question_id=question_id))
+    return render_template('question/question_detail.html', question=question, form=form)
