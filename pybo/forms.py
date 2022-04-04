dd6b847f61a334e849e39969fa41b931039572df
pahkey <pahkey@gmail.com>
2022-04-04 21:57:45 +0900

    3-06

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
 class QuestionForm(FlaskForm):
@@ -11,3 +11,10 @@ class QuestionForm(FlaskForm):
 class AnswerForm(FlaskForm):
     content = TextAreaField('답변 내용', validators=[DataRequired('답변 내용은 필수입력 항목입니다.')])
 
+
+class UserCreateForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('비밀번호', validators=[
+        DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다')])
+    password2 = PasswordField('비밀번호확인', validators=[DataRequired()])
+    email = EmailField('이메일', validators=[DataRequired(), Email()])
