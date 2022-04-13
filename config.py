695eca8037f156ad2002a3d09617e3a83a918fab
pahkey <pahkey@gmail.com>
2022-04-13 13:16:11 +0900

    remove config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
