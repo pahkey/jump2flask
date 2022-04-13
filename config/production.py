731d0d0db1bcf67b61d4e59616f4e5b7556c945e
pahkey <pahkey@gmail.com>
2022-04-13 12:34:12 +0900

    4-17

--- config/production.py
+++ config/production.py
@@ -2,9 +2,14 @@ from logging.config import dictConfig
 
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.environ.get('DB_USER'),
+    pw=os.environ.get('DB_PASSWORD'),
+    url=os.environ.get('DB_HOST'),
+    db=os.environ.get('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
+
 
 dictConfig({
     'version': 1,
