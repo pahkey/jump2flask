fd509b73ba9a0e7e3480d4c464ed723166ecef67
pahkey <pahkey@gmail.com>
2022-04-13 12:47:56 +0900

    4-17

--- config/production.py
+++ config/production.py
@@ -1,15 +1,17 @@
 from logging.config import dictConfig
 
 from config.default import *
+from dotenv import load_dotenv
+
+load_dotenv(BASE_DIR / '.env')
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
-    user=os.environ.get('DB_USER'),
-    pw=os.environ.get('DB_PASSWORD'),
-    url=os.environ.get('DB_HOST'),
-    db=os.environ.get('DB_NAME'))
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'Zb3\x81\xdb\xf1\xd9\xd7-Knb\x8eB\xa5\x18'
-
 
 dictConfig({
     'version': 1,
