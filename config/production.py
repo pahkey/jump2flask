f0a7d588cb82393cf328bf7bf240936435bea181
pahkey <pahkey@gmail.com>
2022-04-13 12:49:46 +0900

    4-17

--- config/production.py
+++ config/production.py
@@ -3,7 +3,7 @@ from logging.config import dictConfig
 from config.default import *
 from dotenv import load_dotenv
 
-load_dotenv(BASE_DIR / '.env')
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     user=os.getenv('DB_USER'),
